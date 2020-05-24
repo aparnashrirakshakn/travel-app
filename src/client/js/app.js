2fe986f3483d9ca0c6e90c61ba057a720cf382fd
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-05-24 14:15:21 -0500

    add api credentials

---
+++ src/client/js/app.js
@@ -0,0 +1,22 @@
+const geoNames = {
+    "baseUrl" : "http://api.geonames.org/searchJSON?",
+    "username" : "<YOUR USER NAME HERE>",
+    "formatted" : true,
+    "style": "full"
+}
+
+const weatherBit = {
+    "baseUrl" : "https://api.weatherbit.io/v2.0/forecast/daily",
+    "key": "<YOUR API KEY HERE>"
+}
+
+const pixabay = {
+    "baseUrl" : "https://pixabay.com/api/",
+    "apiKey": "<YOUR API KEY HERE>",
+    "image_type": "photo"
+}
+
+const restCountries = {
+    "baseUrl": "https://restcountries.eu/rest/v2/name"
+}
+
