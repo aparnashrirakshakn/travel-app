b512ea8a894ef220b05baa305f0e6ad90cd8c660
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-05-24 17:33:05 -0500

    add default image

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,10 @@
 import './styles/style.scss'
 import logo from './assets/logo.png';
+import snap from './assets/snap.png';
 
 
 const applogo = document.getElementById("app-logo");
 applogo.setAttribute("src", logo);
+
+const defaultImage = document.getElementById("card-img-top");
+defaultImage.setAttribute("src", snap);
