e17c03a7256782bf1dae5d588e7a1cc4db57e338
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-05-24 15:03:07 -0500

    construct app skeletion with functions to call

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,21 +1,22 @@
-const API = {
-    geoNames: {
-        "baseUrl" : "http://api.geonames.org/searchJSON?",
-        "username" : "<YOUR USERNAME HERE>",
-        "formatted" : true,
-        "style": "full"
-    },
-    weatherBit: {
-        "baseUrl" : "https://api.weatherbit.io/v2.0/forecast/daily",
-        "key": "<YOUR API KEY HERE>"
-    },
-    pixabay: {
-        "baseUrl" : "https://pixabay.com/api/",
-        "key": "<YOUR API KEY HERE>",
-        "image_type": "photo"
-    },
-    restCountries: {
-        "baseUrl": "https://restcountries.eu/rest/v2/name"
-    }
-}
+import { fetchLocationInfo, fetchWeatherInfo, fetchLocationPic, getCountdown } from '../js/utility';
 
+async function onClickPlanButtonListener(e) {
+    e.preventDefault();
+
+    // fetch user inputs
+
+    const location = document.getElementById('location').value;
+    const tripDate = document.getElementById('trip-date').value;
+
+    const daysToTrip = getCountdown(tripDate);
+
+    // get required info from APIs
+
+    const locationDetails = await fetchLocationInfo(location);
+    const weatherDetails = await fetchWeatherInfo(locationDetails.lat, locationDetails.long);
+    const locationPic = await fetchLocationPic(weatherDetails.countryName);
+
+    // construct the UI
+
+    constructUI(locationDetails, weatherDetails, locationPic);
+}
