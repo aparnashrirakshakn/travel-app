46f3a68757fafc1b50222dfee1ececb802a3d8b4
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-05-24 15:46:34 -0500

    define all fetch functions

--- src/client/js/utility.js
+++ src/client/js/utility.js
@@ -1,15 +1,61 @@
 import { API } from './api';
 
-function fetchLocationInfo(city) {
-    
+async function fetchLocationInfo(city) {
+    const locationUrl = `${API.geoNames.baseUrl}&q=${city}&username=${API.geoNames.username}&style=${API.geoNames.style}`;
+
+    try {
+        const response = fetch(locationUrl);
+
+        if(response.status === 200) {
+            const jsonResponse = await response.json();
+            return jsonResponse.geonames[0];
+        }
+        else {
+            console.log(`Error occured. No valid response received. ${response}`)
+        }
+    }
+    catch(error) {
+        console.log(`Could not fetch location info : ${error}`);
+    }
+
 }
 
 function fetchWeatherInfo(latitude, longitude) {
+    const weatherUrl = `${API.weatherBit.baseUrl}&lat=${latitude}&lon=${longitude}&key=${API.weatherBit.key}`;
 
+    try {
+        const response = fetch(weatherUrl);
+
+        if(response.status === 200) {
+            const jsonResponse = await response.json();
+            return jsonResponse.data[0];
+        }
+        else {
+            console.log(`Error occured. No valid response received. ${response}`)
+        }
+    }
+    catch(error) {
+        console.log(`Could not fetch weather info : ${error}`);
+    }
 }
 
 function fetchLocationPic(countryName) {
+    const locationPicUrl = `${API.pixabay.baseUrl}&q=${city}&key=${API.pixabay.apiKey}&image_type=${API.pixabay.image_type}`;
 
+    try {
+        const response = fetch(locationPicUrl);
+
+        if(response.status === 200) {
+            const jsonResponse = await response.json();
+            return jsonResponse.hits[0].largeImageURL;
+        }
+        else {
+            console.log(`Error occured. No valid response received. ${response}`)
+        }
+    }
+    catch(error) {
+        console.log(`Could not fetch location pic : ${error}`);
+    }
 }
 
 function getCountdown(tripDate) {
