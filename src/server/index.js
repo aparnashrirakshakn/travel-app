44ec20560e7bd7ed92f76af5defa8a37d1cdd5d7
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-05-24 13:46:13 -0500

    add express server

---
+++ src/server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+dotenv.config();
+
+app.use(express.static('dist'));
+
+const port = 8081;
+app.listen(port, listener);
+
+function listener() {
+    console.log(`Server is listening on port ${port}`);
+}
+
+
