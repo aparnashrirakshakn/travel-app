e1d9265f92b80c22b041bd033808a692f17bec82
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-05-24 15:11:42 -0500

    add getCountdown definition

--- src/client/js/utility.js
+++ src/client/js/utility.js
@@ -13,7 +13,10 @@ function fetchLocationPic(countryName) {
 }
 
 function getCountdown(tripDate) {
-
+    var today = new Date();
+    var tripDay = new Date(tripDate);
+    var oneDay = 86400000; //number of milliseconds in a day
+    return (tripDay-today) / oneDay;
 }
 
 export { fetchLocationInfo, fetchWeatherInfo, fetchLocationPic, getCountdown };
