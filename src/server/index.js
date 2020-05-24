3484967f89eee8ee809c059f5939b558a79b5830
Aparna Shriraksha K.N <61300593+aparnashrirakshakn@users.noreply.github.com>
2020-05-24 14:12:25 -0500

    Add server (#5)
    * add express server
    
    * add test endpoint
    
    * add endpoints and listeners
    
    * add endpoint and listener for get plans

---
+++ src/server/index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+dotenv.config();
+
+app.use(express.static('dist'));
+
+const port = 8081;
+app.listen(port, appListener);
+
+// global variables
+let plans = [];
+
+/**
+ * Listeners
+ */
+
+// app
+const appListener = () => {
+    console.log(`Server is listening on port ${port}`);
+}
+
+// test
+const testListener = (req, res) => {
+    res.status(200).send("Test Successful!");
+};
+
+// get
+const renderListener = (req, res) => {
+    res.sendFile('dist/index.html');
+}
+
+// save
+const savePlanListener = (req, res) => {
+    if(req.body) {
+      plans.push(req.body.plan);  
+    }
+    else {
+        res.status(400).send("Invalid request. No plan details available.");
+    }
+};
+
+// get plans
+const getPlansListener = (req, res) => {
+    res.status(200).send(plans);
+}
+
+
+/*
+ * Endpoints
+ */
+
+// test
+app.get('/test', testListener);
+
+// render
+app.get('/', renderListener);
+
+// save
+app.post('/plan/save', savePlanListener);
+
+// get plans
+app.get('/plans', getPlansListener);
