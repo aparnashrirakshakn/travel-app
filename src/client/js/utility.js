4a6f5dea40829df7723d045188a58e9ef2c9f68c
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-05-24 15:03:41 -0500

    construct utility functions skeleton

---
+++ src/client/js/utility.js
@@ -0,0 +1,19 @@
+import { API } from './api';
+
+function fetchLocationInfo(city) {
+    
+}
+
+function fetchWeatherInfo(latitude, longitude) {
+
+}
+
+function fetchLocationPic(countryName) {
+
+}
+
+function getCountdown(tripDate) {
+
+}
+
+export { fetchLocationInfo, fetchWeatherInfo, fetchLocationPic, getCountdown };
